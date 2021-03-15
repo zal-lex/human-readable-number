f2a98d08a1de4be483ae91a2eab65b4697ca4495
Alexey Zalyotov <alexey.zalyotov@rubyroidlabs.com>
2021-03-15 23:58:59 +0300

    feat: implement human readable number function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,24 @@
 module.exports = function toReadable (number) {
-  
+  const units = ['zero', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine'];
+  const firstDecimal = ['ten', 'eleven', 'twelve', 'thirteen', 'fourteen', 'fifteen', 'sixteen', 'seventeen', 'eighteen', 'nineteen'];
+  const decimals = ['', '', 'twenty', 'thirty', 'forty', 'fifty', 'sixty', 'seventy', 'eighty', 'ninety'];
+  const hundred = units.map( (unit) => `${unit} hundred`);
+  const dictionary = [units, decimals, hundred];
+  const digitsStringsArray = number.toString().split('');
+  const reversedDigits = digitsStringsArray.map((digit) => parseInt(digit)).reverse();
+  let result = [];
+  reversedDigits.forEach( (digit, index) => {
+    if (digit === 0) {
+      if (number === 0) {
+        result.unshift(dictionary[index][digit]);
+      }
+    } else if (reversedDigits[1] === 1 && index === 0) {
+      return;
+    } else if (reversedDigits[1] === 1 && index === 1) {
+      result.unshift(firstDecimal[reversedDigits[0]]);
+    } else {
+      result.unshift(dictionary[index][digit]);
+    }
+  })
+  return result.join(' ');
 }
